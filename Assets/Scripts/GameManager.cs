27283f7eabb346af0ced621c224c72bc3c9d890e
Animator1 <39936619+Animator1@users.noreply.github.com>
2019-10-01 22:57:29 +1000

    Added block falling/rotation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private float timer = 0.0f;
+    [SerializeField]
+    private float interval = 1.0f;
+    private float counter = 0.0f;
+
+    [SerializeField]
+    private GameObject activeBlock;
+
+    [SerializeField]
+    private GameObject nextBlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // This moves the active tetromino down over time
+        timer += Time.deltaTime;
+        if (timer > counter)
+        {
+            counter += interval;
+            activeBlock.GetComponent<Tetromino_Behaviour>().StepDown();
+        }
+    }
+
+    public void RotateActiveBlock()
+    {
+        activeBlock.GetComponent<Tetromino_Behaviour>().Rotate();
+    }
+
+
+}
---
+++ Assets/Scripts/Tetromino_Behaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino_Behaviour : MonoBehaviour
+{
+
+    private bool isActive = false;
+    private List<Block_Behaviour> scripts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scripts = new List<Block_Behaviour>();
+        Block_Behaviour[] list = GetComponentsInChildren<Block_Behaviour>();
+        foreach (Block_Behaviour item in list)
+        {
+            scripts.Add(item);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StepDown()
+    {
+        transform.Translate(new Vector3(0.0f, -0.64f), Space.World);
+    }
+
+    public void Rotate()
+    {
+        transform.Rotate(new Vector3(0, 0, 90), Space.Self);  
+        foreach (Block_Behaviour block in scripts)
+        {
+            block.Rotate();
+        }
+    }
+}
